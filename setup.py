ca29c77b93cb86cce5016bf216dd2632c1d9c76f
prenone <achille@achilleme.com>
2024-10-15 00:42:04 +0200

    Fixed leading zeros removal. Was removing EVERY zero, even those trailing nonzero numbers. For example 1200 -> 12

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='NormLatexStr',
-    version='0.1.0',
+    version='0.1.1',
     author='Achille Merendino',
     author_email='achille@achilleme.com',
     description='A package to format numbers with their uncertainties in LaTeX using the SIunitx package.',
